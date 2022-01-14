ff679e4daf0f48bfa9fbe5bc58ea59594647f693
younixue3 <bussiness.needs.rctka@gmail.com>
2022-01-14 16:54:55 +0800

    Fixing

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/css/main.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -32,6 +34,7 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxt/postcss8',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -47,5 +50,12 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <Tutorial />
+  <Header />
+<!--  <Tutorial />-->
 </template>
 
 <script>
